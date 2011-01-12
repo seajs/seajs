da77bb9e6a5e7dfc7679a8c8b94d34534e618904
lifesinger <lifesinger@gmail.com>
2011-01-12 17:46:57 +0800

    bug fix ie7 node.getAttribute(src, 4)

---
+++ src/bootstrap/test/main.js
@@ -0,0 +1,35 @@
+
+// initialize module loader
+var path = location.href.split('/').slice(0, -1).join('/');
+S.reset(path + '/' + decodeURIComponent((location.search || '?').substring(1)));
+
+// add 'test' module
+S.declare('test', [], function(require, exports) {
+
+  exports.print = function(txt, style) {
+    sendMessage('printResults', txt, style);
+  };
+
+  exports.assert = function (guard, message) {
+    if (typeof message === 'undefined') message = '';
+    if (guard) {
+      exports.print('PASS ' + message, 'pass');
+    } else {
+      exports.print('FAIL ' + message, 'fail');
+    }
+  };
+});
+
+S.provide(['program'], function(require) {
+  //try {
+    require('program');
+  //} catch (x) {
+  //  sendMessage('printResults', 'ERROR ' + x.message, 'error');
+  //}
+  sendMessage('testNext');
+});
+
+function sendMessage(msg, a1, a2) {
+  var p = window.parent;
+  if (p && p[msg]) p[msg](a1, a2);
+}
