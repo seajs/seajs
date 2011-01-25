94a3a5267b94fdc5e24645eed90e8e7a94a9a6d4
lifesinger <lifesinger@gmail.com>
2011-01-25 16:58:39 +0800

    start coding for prefix

--- src/module/test/modules/prefix/program.js
+++ src/module/test/modules/prefix/program.js
@@ -2,13 +2,13 @@
 module
     .prefix('github', 'https://github.com/seajs/seajs/raw/master/src/module/test/modules/prefix/submodules')
     .prefix('sub', 'submodule/sub')
-    .declare(['test', 'a', 'github/b', 'sub/c', '~/test/prefix/d'], function(require) {
+    .declare(function(require) {
 
-  var test = require('test');
-  var a = require('a');
-  var b = require('b');
-  var c = require('c');
-  var d = require('d');
+  var test = require('test/test');
+  var a = require('./a');
+  var b = require('github/b');
+  var c = require('sub/c');
+  var d = require('test/modules/prefix/d');
 
   test.assert(a.foo == 'a', 'a.foo should equal to "a".');
   test.assert(b.foo == 'b', 'b.foo should equal to "b".');
