df5741ee9a43d8e9e3a83a61dca1c77daa3cdadc
lifesinger <lifesinger@gmail.com>
2011-04-03 12:21:18 +0800

    delete package demo

--- src/module/demo/package/build/main.js
+++
@@ -1,6 +0,0 @@
-module.declare('main', ['./biz/increment'], function(require) {
-
-  var inc = require('./biz/increment').increment;
-  document.body.innerHTML = 'inc(1) = ' + inc(1);
-
-});
--- src/module/demo/package/src/main.js
+++
@@ -1,6 +0,0 @@
-module.declare(function(require) {
-
-  var inc = require('./biz/increment').increment;
-  document.body.innerHTML = 'inc(1) = ' + inc(1);
-
-});
