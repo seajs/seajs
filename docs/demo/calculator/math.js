501cb51f8fcc37b11841f666da20d5b6a74f3582
lifesinger <lifesinger@gmail.com>
2011-05-08 16:05:53 +0800

    add calculator demo

---
+++ docs/demo/calculator/math.js
@@ -0,0 +1,20 @@
+
+define(function(require, exports) {
+
+  exports.add = function(n, m) {
+    return n + m;
+  };
+
+  exports.subtract = function(n, m) {
+    return n - m;
+  };
+
+  exports.multiple = function(n, m) {
+    return n * m;
+  };
+
+  exports.divide = function(n, m) {
+    return n / m;
+  };
+
+});
