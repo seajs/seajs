c5096da726f58a123f2f7c0ac113df356ec09d98
lifesinger <lifesinger@gmail.com>
2011-09-27 22:09:13 +0800

    add xxlib test case

--- test/issues/config-base/main.js
+++ test/issues/config-base/main.js
@@ -5,10 +5,12 @@ define(function(require) {
   var a = require('a');
   var b = require('biz/b');
   var c = require('biz/sub/c');
+  var xxlib = require('xxlib');
 
   test.assert(a.a === 'a', 'a is ok');
   test.assert(b.b === 'b', 'b is ok');
   test.assert(c.c === 'c', 'c is ok');
+  test.assert(xxlib.name === 'xxlib', 'xxlib is ok');
 
   test.done();
 
---
+++ test/issues/config-base/xxlib/1.0.0/xxlib.js
@@ -0,0 +1,3 @@
+define('xxlib', function(require, exports) {
+  exports.name = 'xxlib';
+});
