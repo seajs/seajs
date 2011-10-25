f7548beca0342d4c8edf4d86f94e4bd67510163f
lifesinger <lifesinger@gmail.com>
2011-10-25 10:48:25 +0800

    add coffee test case

---
+++ test/issues/coffee/a.coffee
@@ -0,0 +1,2 @@
+define (require, exports) ->
+  exports.name = 'a'
---
+++ test/issues/coffee/main.js
@@ -0,0 +1,7 @@
+define(function(require) {
+  var test = require('../../test');
+  var a = require('./a.coffee');
+
+  test.assert(a.name === 'a', 'coffee script is ok');
+  test.done();
+});
