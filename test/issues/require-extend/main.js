0cdfeaee6e6ab9cf8e37f44e467cf322fe0b809a
lifesinger <lifesinger@gmail.com>
2011-10-25 13:19:49 +0800

    update test cases. close #102

--- test/issues/require-extend/main.js
+++ test/issues/require-extend/main.js
@@ -1,9 +1,22 @@
 define(function(require) {
-
   var test = require('../../test');
-  var a = require('./a.coffee');
 
-  test.assert(a.name === 'a', 'a.coffee is ok');
-  test.done();
+
+  // before
+  var r = require.resolve('./a.coffee');
+  test.assert(/\.js$/.test(r), r);
+
+  // run extend
+  require('./extend');
+
+  // after
+  r = require.resolve('./a.coffee');
+  test.assert(/\.coffee$/.test(r), r);
+
+
+  require.async('./b', function(a) {
+    test.assert(a.name === 'a', 'a.coffee is ok');
+    test.done();
+  });
 
 });
