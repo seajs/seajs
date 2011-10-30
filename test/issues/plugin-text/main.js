8037ba9295e27984e1f7c2d83235f50dfb0b891c
lifesinger <lifesinger@gmail.com>
2011-10-30 11:27:03 +0800

    move json support to plugin-json

--- test/issues/plugin-text/main.js
+++ test/issues/plugin-text/main.js
@@ -3,13 +3,10 @@ define(function(require) {
   var test = require('../../test');
 
   var tpl = require('./a.tpl');
-  var json = eval('(' + require('./b.json') + ')');
   var html = require('./c.html');
 
   test.assert(tpl === 'I am a template file. "\'', tpl);
-  test.assert(json.name === 'b', json.name);
-  test.assert(json.foo === "'bar'\"", json.foo);
   test.assert(~html.indexOf('I am a html file.'), 'html is ok');
-  test.done();
 
+  test.done();
 });
