1340cd9417bc4ceb7959e36b83efefab1a8e1cdf
lifesinger <lifesinger@gmail.com>
2011-11-02 10:38:20 +0800

    improve test cases for real src

--- test/issues/debug/main.js
+++ test/issues/debug/main.js
@@ -20,10 +20,19 @@ define(function(require) {
 
   test.assert(debugInitialValue === undefined, 'initial debug value is undefined');
   test.assert(seajs.debug === 1, 'seajs.debug should be 1');
-  test.assert(a.id.indexOf(timestamp) > 0, 'a is ok');
-  test.assert(b.id.indexOf(timestamp) > 0, 'b is ok');
-  test.assert(c.id.indexOf(timestamp) > 0, 'c is ok');
-  test.assert(d.id.indexOf(timestamp) > 0, 'd is ok');
+  test.assert(a.id.indexOf(timestamp) === -1, a.id);
+  test.assert(b.id.indexOf(timestamp) === -1, b.id);
+  test.assert(c.id.indexOf(timestamp) === -1, c.id);
+  test.assert(d.id.indexOf(timestamp) === -1, d.id);
+
+
+  var scripts = document.getElementsByTagName('script');
+  for (var i = 0; i < scripts.length; i++) {
+    var src = scripts[i].src;
+    if (src && /\/debug\/\w\.js/.test(src)) {
+      test.assert(src.indexOf(timestamp) > 0, src);
+    }
+  }
 
   test.done();
 });
--- test/issues/multi-map/a.js
+++ test/issues/multi-map/a.js
@@ -1,8 +1,18 @@
 define(function(require, exports, module) {
 
-  var test = require('../../test');
-
-  test.assert(/\?1\?2\?3$/.test(module.id), module.id);
   exports.name = 'a';
+
+  var test = require('../../test');
+  test.assert(/a\.js$/.test(module.id), module.id);
+
+
+  var scripts = document.getElementsByTagName('script');
+
+  for (var i = 0; i < scripts.length; i++) {
+    var src = scripts[i].src;
+    if (src && /\/a\.js/.test(src)) {
+      test.assert(/\?1\?2\?3/.test(src), src);
+    }
+  }
 
 });
