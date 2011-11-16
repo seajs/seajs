cfdee9a51c1a4a9922c44c9a1a497bf2b3a5b222
lifesinger <lifesinger@gmail.com>
2011-11-16 17:17:41 +0800

    adjust for new packing method

--- test/packages/math/program.js
+++ test/packages/math/program.js
@@ -1,3 +1,4 @@
-define("./program",["../../test","./increment"],function(a){var b=a("../../test"),c=a("./increment").increment;b.assert(c(1)===2,"The result of inc(1) is 2."),b.done()});
-define("./increment",["./math"],function(a,b){var c=a("./math").add;b.increment=function(a){return c(a,1)}});
-define("./math",[],function(a,b){b.add=function(){var a=0,b=0,c=arguments.length;while(b<c)a+=arguments[b++];return a}});
+seajs.config({ alias: { 's_auto_pkg_1': location.href.replace(/(.*\/)([^\/]+)/, '$1') + 'packages/math' } });
+define("s_auto_pkg_1/program",["../../test","./increment"],function(a){var b=a("../../test"),c=a("./increment").increment;b.assert(c(1)===2,"The result of inc(1) is 2."),b.done()});
+define("s_auto_pkg_1/increment",["./math"],function(a,b){var c=a("./math").add;b.increment=function(a){return c(a,1)}});
+define("s_auto_pkg_1/math",[],function(a,b){b.add=function(){var a=0,b=0,c=arguments.length;while(b<c)a+=arguments[b++];return a}});
