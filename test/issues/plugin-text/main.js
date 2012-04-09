4e7e3a0a1247af0462eaaff5d876a211312299c4
lifesinger <lifesinger@gmail.com>
2012-04-09 22:55:15 +0800

    improve test case for plugin-text. close #176

--- test/issues/plugin-text/main.js
+++ test/issues/plugin-text/main.js
@@ -8,11 +8,15 @@ define(function(require) {
 
   var test = require('../../test');
 
-  var tpl = require('./a.tpl');
+  var a = require('./a.tpl');
+  var b = require('./b.tpl');
+  var c = require('./c.tpl');
   var html = require('./c.html');
 
-  test.assert(tpl === 'I am a template file. "\'', tpl);
-  test.assert(~html.indexOf('I am a html file.'), 'html is ok');
+  test.assert(a === 'I am a template file. "\'', 'a is ok');
+  test.assert(b.length === 0, 'b is ok');
+  test.assert(c.indexOf('</div>') > 0, 'c is ok');
+  test.assert(html.indexOf('I am a html file.') > 0, 'html is ok');
 
   test.done();
 });
