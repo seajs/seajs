70f61b8098643fdd0ad130a5de33e91c237c97ef
lifesinger <lifesinger@gmail.com>
2012-04-24 14:19:23 +0800

    change build directory to dist

--- tools/size.sh
+++ tools/size.sh
@@ -4,9 +4,9 @@
 SRC=sea-debug.js
 MIN=sea.js
 
-SIZE_SRC=$(cat build/$SRC | wc -c)
-SIZE_MIN=$(cat build/$MIN | wc -c)
-SIZE_GZIP=$(gzip -nfc --best build/$MIN | wc -c)
+SIZE_SRC=$(cat dist/$SRC | wc -c)
+SIZE_MIN=$(cat dist/$MIN | wc -c)
+SIZE_GZIP=$(gzip -nfc --best dist/$MIN | wc -c)
 
 echo "    `echo "scale=3;$SIZE_SRC/1024" | bc -l` bytes $SRC"
 echo "    `echo "scale=3;$SIZE_MIN/1024" | bc -l` bytes $MIN"
