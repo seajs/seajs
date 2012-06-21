d397be380ded9c0a7b624b5ab3a314ff952ca353
lifesinger <lifesinger@gmail.com>
2012-06-21 18:21:02 +0800

    refator plugin-base

--- src/plugins/plugin-base.js
+++ src/plugins/plugin-base.js
@@ -1,117 +1,117 @@
-
 /**
- * @fileoverview The base utilities for plugin development.
+ * The base utilities for plugin development
  */
+define('seajs/plugin-base', [], function(require, exports) {
 
-define('plugin-base', [], function(require, exports) {
+  var pluginSDK = seajs.pluginSDK
+  var util = pluginSDK.util
+  var Module = pluginSDK.Module
 
-  var util = seajs.pluginSDK.util;
-  var RP = require.constructor.prototype;
-  var meta = {};
-  var cache = {};
+  var meta = {}
+  var cache = {}
 
 
   exports.add = function(o) {
-    meta[o.name] = o;
-  };
+    meta[o.name] = o
+  }
 
 
   exports.util = {
     xhr: xhr,
     globalEval: globalEval
-  };
+  }
 
 
-  extendResolve();
-  extendLoad();
+  extendResolve()
+  extendLoad()
 
 
   function extendResolve() {
-    var _resolve = RP.resolve;
+    var _resolve = Module._resolve
 
-    RP.resolve = function(id, context) {
+    Module._resolve = function(id, context) {
       if (util.isArray(id)) {
-        return _resolve(id, context);
+        return _resolve(id, context)
       }
 
-      var flag;
+      var flag
 
       if (/\.\w|^\w+!/.test(id)) {
-        var m;
+        var m
 
         // id = text!path/to/some
         if ((m = id.match(/^(\w+)!(.*)$/))) {
-          flag = m[1];
-          id = m[2];
+          flag = m[1]
+          id = m[2]
         }
 
         // id = abc.xyz?t=123
         else if ((m = id.match(/[^?]*(\.\w+)/))) {
-          var ext = m[1];
+          var ext = m[1]
           for (var type in meta) {
             if (meta.hasOwnProperty(type) &&
                 ~meta[type].ext.join('|').indexOf(ext)) {
-              flag = type;
-              break;
+              flag = type
+              break
             }
           }
         }
 
         // don't add .js
         if (flag && !/\?|#$/.test(id)) {
-          id += '#';
+          id += '#'
         }
       }
 
-      var uri = _resolve.call(this, id, context);
+      var uri = _resolve.call(this, id, context)
 
       if (flag && meta[flag] && !cache[uri]) {
-        cache[uri] = flag;
+        cache[uri] = flag
       }
-      return uri;
+      return uri
     };
   }
 
 
   function extendLoad() {
-    var _load = RP.load;
+    var _load = RP.load
 
     RP.load = function(url, callback, charset) {
-      var type = cache[util.unParseMap(url)];
+      var type = cache[util.unParseMap(url)]
       if (type) {
-        meta[type].load(url, callback);
+        meta[type].load(url, callback)
       }
       else {
-        _load(url, callback, charset);
+        _load(url, callback, charset)
       }
-    };
+    }
   }
 
 
   function xhr(url, callback) {
-    var r = new (window.ActiveXObject || XMLHttpRequest)('Microsoft.XMLHTTP');
-    r.open('GET', url, true);
+    var r = new (window.ActiveXObject || XMLHttpRequest)('Microsoft.XMLHTTP')
+    r.open('GET', url, true)
 
     r.onreadystatechange = function() {
       if (r.readyState === 4) {
         if (r.status === 200) {
-          callback(r.responseText);
+          callback(r.responseText)
         }
         else {
-          throw 'Could not load: ' + url + ', status = ' + r.status;
+          throw new Error('Could not load: ' + url + ', status = ' + r.status)
         }
       }
-    };
+    }
 
-    return r.send(null);
+    return r.send(null)
   }
 
 
   function globalEval(data) {
     if (data && /\S/.test(data)) {
       ( window.execScript || function(data) {
-        window['eval'].call(window, data);
-      } )(data);
+        window['eval'].call(window, data)
+      } )(data)
     }
   }
 
