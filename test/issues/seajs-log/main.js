152090db2e7333f97d6c47d8ebee9b48d7fdcfd7
lifesinger <lifesinger@gmail.com>
2012-06-23 11:35:01 +0800

    improve output using group

--- test/issues/seajs-log/main.js
+++ test/issues/seajs-log/main.js
@@ -2,6 +2,8 @@ define(function(require) {
 
   var test = require('../../test')
 
+  test.assert(seajs.log('seajs.log test', 'group') === undefined, 'group')
+
   test.assert(seajs.log('a') === undefined, 'log')
   test.assert(seajs.log('a', 'b') === undefined, 'log')
   test.assert(seajs.log('a', 'b', 1) === undefined, 'log')
@@ -9,14 +11,14 @@ define(function(require) {
   test.assert(seajs.log('a', 'warn') === undefined, 'warn')
   test.assert(seajs.log('a', 'info') === undefined, 'info')
   test.assert(seajs.log('a', 'error') === undefined, 'error')
-  test.assert(seajs.log('a', 'group') === undefined, 'group')
-  test.assert(seajs.log('a', 'groupEnd') === undefined, 'groupEnd')
   test.assert(seajs.log('a', 'time') === undefined, 'time')
   test.assert(seajs.log('a', 'timeEnd') === undefined, 'timeEnd')
 
   test.assert(seajs.log('a', 'dir') === undefined, 'dir')
   test.assert(seajs.log({ name: 'a' }, 'dir') === undefined, 'dir')
 
+  test.assert(seajs.log('seajs.log test', 'groupEnd') === undefined, 'groupEnd')
+
   test.done()
-  
+
 })
