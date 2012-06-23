583e6c0d606eece43fe1625873e6f03e0be5ee03
lifesinger <lifesinger@gmail.com>
2012-06-23 16:42:37 +0800

    make sure the test case is robust

--- test/issues/seajs-find/main.js
+++ test/issues/seajs-find/main.js
@@ -6,11 +6,11 @@ define(function(require) {
   require('./b-xx')
 
 
-  test.assert(seajs.find('a-xx.js').name === 'a', 'find("a-xx.js")')
-  test.assert(seajs.find('xx')[0].name === 'a', 'find("xx")[0].name')
-  test.assert(seajs.find('xx')[1].name === 'b', 'find("xx")[1].name')
-  test.assert(seajs.find('xx').length === 2, 'find("xx").length')
-  test.assert(seajs.find('zz') === null, 'find("zz")')
+  test.assert(seajs.find('a-xx.js').name === 'a', seajs.find("a-xx.js"))
+  test.assert(seajs.find('xx')[0].name.length === 1, seajs.find("xx")[0].name)
+  test.assert(seajs.find('xx')[1].name.length === 1, seajs.find("xx")[1].name)
+  test.assert(seajs.find('xx').length === 2, seajs.find("xx").length)
+  test.assert(seajs.find('zz') === null, seajs.find("zz"))
 
   test.done()
 
