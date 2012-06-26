f456d77c4321f617b56aca73374002d0a0672810
lifesinger <lifesinger@gmail.com>
2012-06-26 10:24:06 +0800

    throw error when the combo url is too long. close #230

--- dist/plugin-combo.js
+++ dist/plugin-combo.js
@@ -199,8 +199,14 @@ define('seajs/plugin-combo', function() {
     util.forEach(paths, function(path) {
       var root = path[0] + '/'
       var parts = path[1]
-      var comboPath = root + comboSyntax[0] + parts.join(comboSyntax[1])
+
       var hash = {}
+      var comboPath = root + comboSyntax[0] + parts.join(comboSyntax[1])
+      
+      // http://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url
+      if (comboPath.length > 2000) {
+        throw new Error('The combo url is too long: ' + comboPath)
+      }
 
       util.forEach(parts, function(part) {
         hash[root + part] = comboPath
