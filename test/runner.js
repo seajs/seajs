ec06ba05d7f623362c19555cd38d0f59c89d35f0
lifesinger <lifesinger@gmail.com>
2012-06-27 10:28:27 +0800

    Tweak code style

--- test/runner.js
+++ test/runner.js
@@ -1,36 +1,35 @@
 /**
- * Run test cases in node environment.
- * @author lifesinger@gmail.com (Frank Wang)
+ * Run test cases in node environment
  */
 
-var path = require('path');
-require('../lib/sea-node');
+var path = require('path')
+require('../lib/sea-node')
 
 
-var test = require('./test');
-var testCases = require('./config').testCases;
+var test = require('./test')
+var testCases = require('./config').testCases
 
 var excludes = [
   'modules/configMap'
   ,'modules/define'
   ,'modules/preload'
-];
+]
 
 testCases = testCases.filter(function(testCase) {
   return testCase.indexOf('modules/') === 0 &&
-      excludes.indexOf(testCase) === -1;
-});
+      excludes.indexOf(testCase) === -1
+})
 
 
 test.next = function() {
-  var testCase;
+  var testCase
 
   while ((testCase = testCases.shift())) {
-    test.print(testCase, 'info');
-    require(path.join(__dirname, testCase, 'program.js'));
+    test.print(testCase, 'info')
+    require(path.join(__dirname, testCase, 'program.js'))
   }
-};
+}
 
 
 // go
-test.next();
+test.next()
