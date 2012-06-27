8eeb5462da27b2a69f5a63d6324eaf961659d220
lifesinger <lifesinger@gmail.com>
2012-06-27 13:22:48 +0800

    Remove hard code in dom-ready test case

--- test/issues/dom-ready/main.js
+++ test/issues/dom-ready/main.js
@@ -1,23 +1,8 @@
-var MODULES_PATH = 'http://modules.seajs.org/'
-var isLocal = location.href.indexOf('/~lifesinger/') > 0
-
-if (isLocal) {
-  MODULES_PATH = 'http://' + location.host + '/~lifesinger/seajs/spm/modules/'
-}
-
-seajs.config({
-  base: MODULES_PATH,
-
-  alias: {
-    'jquery': '1.7.2'
-  }
-});
-
-
 define(function(require) {
 
   var test = require('../../test')
-  var $ = require('jquery')
+  var $ = require('../../third-party').jQuery
+
 
   $(document).ready(function() {
 
