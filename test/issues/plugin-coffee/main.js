a23e5071b32416f0e58422dff7075b8a054ae01a
lifesinger <lifesinger@gmail.com>
2012-06-27 13:58:20 +0800

    Move the declaration of coffee and less to third-party file

--- test/issues/plugin-coffee/main.js
+++ test/issues/plugin-coffee/main.js
@@ -1,15 +1,15 @@
 define(function(require) {
 
-  var test = require('../../test');
+  var test = require('../../test')
 
-  var a = require('./a2.coffee');
-  var b = require('./b.coffee');
-  var c = require('coffee!./c.coffee');
+  var a = require('./a2.coffee')
+  var b = require('./b.coffee')
+  var c = require('coffee!./c.coffee')
 
-  test.assert(a.name === 'a', a.name);
-  test.assert(b.name === 'b', b.name);
-  test.assert(c.name === 'c', c.name);
-  test.assert(b.a === a, b.a.name);
+  test.assert(a.name === 'a', a.name)
+  test.assert(b.name === 'b', b.name)
+  test.assert(c.name === 'c', c.name)
+  test.assert(b.a === a, b.a.name)
 
-  test.done();
-});
+  test.done()
+})
--- test/third-party.js
+++ test/third-party.js
@@ -1,22 +1,49 @@
+;
+(function() {
 
-seajs.config({
-  map: [
+  var SEAJS_PATH = seajs.pluginSDK.config.base
+  var MODULES_PATH = 'https://a.alipayobjects.com/static/arale/'
+
+  if (location.href.indexOf('/~lifesinger/') > 0) {
+    MODULES_PATH = 'http://' + location.host + '/~lifesinger/seajs/spm/modules/'
+  }
+
+
+  var hash = {
+    'jquery/1.7.2/jquery.js': 'jQuery',
+    'coffee/1.3.3/coffee-script.js': 'CoffeeScript',
+    'less/1.3.0/less.js': 'LESS'
+  }
+
+
+  seajs.config({
+    map: [
       function(url) {
-        var base = seajs.pluginSDK.config.base
-        var online = 'https://a.alipayobjects.com/static/arale/'
-        var jq = 'jquery/1.7.2/jquery.js'
-
-        if (url === base + jq) {
-          return online + jq
+        if (hash[url.substring(SEAJS_PATH.length)]) {
+          return url.replace(SEAJS_PATH, MODULES_PATH)
         }
-
         return url
       }
-  ]
-})
+    ]
+  })
 
-define(function(require, exports) {
 
-  exports.jQuery = require('jquery/1.7.2/jquery')
+  var deps = []
+  var names = []
 
-})
+  for (var key in hash) {
+    if (hash.hasOwnProperty(key)) {
+      deps.push(key)
+      names.push(hash[key])
+    }
+  }
+
+  define(deps, function(require, exports) {
+
+    for (var i = 0; i < names.length; i++) {
+      exports[names[i]] = require(deps[i])
+    }
+
+  })
+
+})()
