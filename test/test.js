922660c2b5309fc28940f09e5bd67cfe9eb95676
lifesinger <lifesinger@gmail.com>
2012-06-27 10:42:59 +0800

    Catch errors in iframe pages

--- test/test.js
+++ test/test.js
@@ -1,68 +1,91 @@
 (function(factory) {
 
   if (typeof define === 'function') {
-    define(factory);
+    define(factory)
   }
   else if (typeof exports !== 'undefined') {
-    factory(require, exports);
+    factory(require, exports)
   }
   else {
-    factory(null, (this['test'] = {}));
+    factory(null, (this['test'] = {}))
   }
 
 })(function(require, exports) {
 
+
+  exports.print = function(txt, style) {
+    sendMessage('printResults', txt, style || 'info')
+  }
+
+
+  exports.assert = function (guard, message) {
+    if (typeof message === 'undefined') {
+      message = ''
+    }
+
+    if (guard) {
+      exports.print('[PASS] ' + message, 'pass')
+    } else {
+      exports.print('[FAIL] ' + message, 'fail')
+    }
+  }
+
+
+  exports.next = function() {
+    setTimeout(function() {
+      sendMessage('testNext')
+    }, 500) // 留 500ms 空隙，使得异步操作能有时间完成。
+  }
+
+
+  exports.done = function() {
+    exports.print('[DONE]')
+    exports.next()
+  }
+
+
+  function error(err) {
+    exports.print('[ERROR] ' + err, 'error')
+    exports.next()
+  }
+
+
+  // Catch errors
+  if (typeof process !== 'undefined') {
+    process.on('uncaughtException', error)
+  }
+  else {
+    window.onerror = error
+  }
+
+
+  // Helpers
+  // -------
+
   function sendMessage(fn, msg, type) {
-    var p = this;
+    var p = this
     if (this != this.parent) {
-      p = this.parent;
+      p = this.parent
     }
 
     if (p && p[fn]) {
-      p[fn](msg, type);
+      p[fn](msg, type)
     }
-    else if (msg && typeof console !== 'undefined') {
-      console.log(color(msg, type));
+    else if (typeof console !== 'undefined') {
+      console.log(color(msg, type))
     }
   }
 
   // https://github.com/loopj/commonjs-ansi-color/blob/master/lib/ansi-color.js
   var ANSI_CODES = {
     'fail': 31, // red
+    'error': 31, // red
     'pass': 32, // green
     'info': 36 // cyan
-  };
-
-  function color(str, type) {
-    return '\033[' + ANSI_CODES[type] + 'm  ' + str + '\033[0m';
   }
 
-
-  exports.print = function(txt, style) {
-    sendMessage('printResults', txt, style || 'info');
-  };
-
-  exports.assert = function (guard, message) {
-    if (typeof message === 'undefined') {
-      message = '';
-    }
-
-    if (guard) {
-      exports.print('[PASS] ' + message, 'pass');
-    } else {
-      exports.print('[FAIL] ' + message, 'fail');
-    }
-  };
-
-  exports.next = function() {
-    setTimeout(function() {
-      sendMessage('testNext');
-    }, 500); // 留 500ms 空隙，使得异步操作能有时间完成。
-  };
-
-  exports.done = function() {
-    exports.print('[DONE]');
-    exports.next();
-  };
+  function color(str, type) {
+    return '\033[' + ANSI_CODES[type] + 'm  ' + str + '\033[0m'
+  }
 
 });
