dd980088bbc241a0f2690117a39e45cece3ed96f
lepture <lepture@me.com>
2012-07-01 23:56:44 +0800

    update github

--- docs/assets/home.js
+++ docs/assets/home.js
@@ -5,7 +5,8 @@
 seajs.config({
   alias: {
     'jquery': 'https://a.alipayobjects.com/static/arale/jquery/1.7.2/jquery.js',
-    'github': 'http://lepture.github.com/github.js/src/github.js'
+    'github': 'http://lepture.github.com/github.js/src/github.js',
+    'github-render': 'http://lepture.github.com/github.js/src/render.js'
   }
 })
 
@@ -13,14 +14,14 @@ define(function(require) {
 
   require('./highlight').init()
 
-  require.async(['jquery', './hello', 'github'], function($, hello, github) {
+  require.async(['jquery', './hello', 'github', 'github-render'], function($, hello, github, render) {
 
     // first example
     $('#beautiful-sea').click(hello.sayHello)
 
     // github items
-    github('seajs/seajs').issues().commits({ perpage: 10 })
-
+    github('seajs/seajs').issues({ callback: render.issues });
+    github('seajs/seajs').commits({ callback: render.commits });
   })
 
 })
