39d711e53b1abae1a5215fe6700e61149c463ecc
lifesinger <lifesinger@gmail.com>
2012-07-16 12:29:52 +0800

    Improve test case

--- test/issues/auto-transport/main.js
+++ test/issues/auto-transport/main.js
@@ -1,8 +1,10 @@
 define(function(require) {
 
   var test = require('../../test')
+  var $ = require('jquery')
   var cookie = require('cookie')
 
+  test.assert($.fn, '$.fn')
   test.assert(typeof cookie === 'function', typeof cookie)
   test.assert(cookie.defaults, 'cookie.defaults')
 
