b6c475c7e3e3148eaabc34640700deabcf09cb23
lifesinger <lifesinger@gmail.com>
2012-08-15 15:22:43 +0800

    Tweak seajs.find test case for recent modification

--- test/issues/seajs-find/main.js
+++ test/issues/seajs-find/main.js
@@ -6,11 +6,11 @@ define(function(require) {
   require('./b-xx')
 
 
-  test.assert(seajs.find('a-xx.js').name === 'a', seajs.find("a-xx.js"))
+  test.assert(seajs.find('a-xx.js')[0].name === 'a', seajs.find("a-xx.js"))
   test.assert(seajs.find('xx')[0].name.length === 1, seajs.find("xx")[0].name)
   test.assert(seajs.find('xx')[1].name.length === 1, seajs.find("xx")[1].name)
   test.assert(seajs.find('xx').length === 2, seajs.find("xx").length)
-  test.assert(seajs.find('zz') === null, seajs.find("zz"))
+  test.assert(seajs.find('zz').length === 0, seajs.find("zz"))
 
   test.done()
 
