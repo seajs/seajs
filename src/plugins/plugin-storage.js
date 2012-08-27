0b8d390be8db7e661e50b393ce5b0f2dc7845153
lifesinger <lifesinger@gmail.com>
2012-08-27 16:55:45 +0800

    Refactor code for plugin-storage

--- src/plugins/plugin-storage.js
+++ src/plugins/plugin-storage.js
@@ -1,62 +1,53 @@
 /**
  * The storage plugin
  */
-define('seajs/plugin-storage', ['./plugin-base','store','manifest'], function(require) {
+define('seajs/plugin-storage', ['./plugin-base', 'store', 'manifest'], function(require) {
 
-  var plugin = require('./plugin-base'),
-      manifest = require('manifest'),
-      util = plugin.util,
-      store = require('store'),
-      s = store.createStorage('localStorage'),
-      _manifest = s.get('manifest'),
-      isNeedUpdate = false,
-      updateList = {}
+  var latestManifest = require('manifest')
+  if (!latestManifest) return
 
-  // need update only when  _manifest does not exist or local manifest.version not equals online manifest.version 
-  isNeedUpdate = !_manifest || _manifest.version !== manifest.version 
+  var plugin = require('./plugin-base')
+  var util = plugin.util
+  var store = require('store').createStorage('localStorage')
 
-  if(isNeedUpdate){
-    // if local manifest does not exist  update all entries which are listed in manifest file
-    if(!_manifest){
-      updateList = manifest
-    }else{
-      for(var i in manifest){
-        if(i != 'version'){
+  var storedManifest = store.get('manifest')
+  var updatedList = {}
 
-          // Update a entry when the entry does not exist in the manifest file or local entry.version not equals online entry.version 
-          
-          !_manifest[i] && (updateList[i] = manifest[i])
 
-          _manifest[i] && _manifest[i].version != manifest[i].version && (updateList[i] = manifest[i])
-        }
+  var isNeedUpdate = !storedManifest ||
+      storedManifest.version !== latestManifest.version
 
-      }
-    }
-    _manifest = manifest
-    s.set('manifest',manifest)
+  if (isNeedUpdate) {
+    updatedList = getUpdatedList()
+
+    // Update manifest in localStorage to the latest version
+    storedManifest = latestManifest
+    store.set('manifest', storedManifest)
   }
 
 
+  // Hack Module._resolve method
   extendResolve()
 
 
+  // Register plugin information
   plugin.add({
     name: 'storage',
 
     ext: ['.js'],
 
     fetch: function(url, callback) {
+      var cachedCode = store.get(url)
 
-      var stCache = s.get(url),
-          realPath= util.toRealPath(url,_manifest)
-      // use localStorage only when the entry has existed in localStorage and the entry is listed in manifest file meanwhile not be listed in updateList
-      if(stCache  && _manifest[url] && !updateList[url]){
-        util.globalEval(stCache)
+      if (cachedCode && storedManifest[url] && !updatedList[url]) {
+        util.globalEval(cachedCode)
         callback()
-      }else{
+      }
+      else {
+        var realPath = getRealPath(url, storedManifest)
 
         util.xhr(realPath, function(code) {
-          parseInt(seajs.pluginSDK.config.debug) == 2 || ( _manifest[url] && s.set(url,code) )
+          parseInt(seajs.pluginSDK.config.debug) == 2 || ( storedManifest[url] && store.set(url, code) )
           util.globalEval(code)
           callback()
         })
@@ -66,31 +57,50 @@ define('seajs/plugin-storage', ['./plugin-base','store','manifest'], function(re
   })
 
 
+  // Helpers
+  // -------
+
+  function getUpdatedList() {
+    if (!storedManifest) {
+      return latestManifest
+    }
+
+    var list = {}
+
+    for (var key in latestManifest) {
+      if (!latestManifest.hasOwnProperty(key) || key === 'version') continue
+
+      var storedItem = storedManifest[key]
+      var latestItem = latestManifest[key]
+
+      if (!storedItem || storedItem.version !== latestItem.version) {
+        list[key] = latestItem
+      }
+    }
+
+    return list
+  }
+
+
   var Module = seajs.pluginSDK.Module
-  
-  // fix refUri
+
   function extendResolve() {
     var _resolve = Module._resolve
-
     Module._resolve = function(id, refUri) {
-      var manifest = Module._find('manifest')
-
-      manifest && (refUri = toRealPath(refUri, manifest))
-      return _resolve(id, refUri)
+      return _resolve(id, getRealPath(refUri, storedManifest))
     }
   }
 
-  // manifest file maybe declare the entry·s version ，we need to make up the real path
-  function toRealPath(url, manifest) {
-    if (!manifest[url]) return url
 
-    var m = url.match(/^(.*)\/(.*)$/)
+  function getRealPath(url, manifest) {
+    var version
+    if (!manifest[url] || !(version = manifest[url]['version'])) return url
+
+    var m = url.match(/^(.*)\/([^\/]+)$/)
+    if (!m) return url
+
     var dirname = m[1]
     var name = m[2]
-
-    var version = manifest[url]['version']
-    if (!version) return url
-
     return dirname + '/' + version + '/' + name
   }
 
