654342efa7f3fd196c7029f3df137171b02bf231
lifesinger <lifesinger@gmail.com>
2012-12-15 21:50:51 +0800

    Add test page for documentFragment

--- tests/research/doc-fragment/test.js
+++ tests/research/doc-fragment/test.js
@@ -5,6 +5,7 @@ var Z = 'script-z'
 print('X = ' + (typeof X === 'undefined' ? 'undefined' : X))
 print('Y = ' + (typeof Y === 'undefined' ? 'undefined' : Y))
 print('Z = ' + (typeof Z === 'undefined' ? 'undefined' : Z))
+
 print('window.X = ' + window.X)
 
 print('----- Script END -----')
