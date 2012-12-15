56a1e11813330b4057b3edbd9b385256607e1cb0
lifesinger <lifesinger@gmail.com>
2012-12-15 21:49:52 +0800

    Turn on all test issues

---
+++ tests/research/doc-fragment/test.js
@@ -0,0 +1,10 @@
+print('----- Script BEGIN -----')
+
+var Z = 'script-z'
+
+print('X = ' + (typeof X === 'undefined' ? 'undefined' : X))
+print('Y = ' + (typeof Y === 'undefined' ? 'undefined' : Y))
+print('Z = ' + (typeof Z === 'undefined' ? 'undefined' : Z))
+print('window.X = ' + window.X)
+
+print('----- Script END -----')
