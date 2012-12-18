fc808e1a41faffa15df66ac15155c314d7231c1a
XiNGRZ <chenxingyu92@gmail.com>
2012-12-18 23:46:42 +0800

    进一步简化代码

--- src/util-deps.js
+++ src/util-deps.js
@@ -3,39 +3,23 @@
  */
 ;(function(util) {
 
-  var REQUIRE_RE = /(?:^|[^.$])\brequire\s*\(\s*(["'])([^"'\s\)]+)\1\s*\)/g
-
-
   util.parseDependencies = function(code) {
-    // Parse these `requires`:
-    //   var a = require('a');
-    //   someMethod(require('b'));
-    //   require('c');
-    //   ...
-    // Doesn't parse:
-    //   someInstance.require(...);
-    var ret = [], _strings = [], match;
+    var ret = [], _strings = [];
 
-    // extract all strings
-    code = code.replace(/(["'])(.+?)\1/g, function(match, quote, str, index) {
-      _strings.push(str);
-      return match.replace(str, _strings.length - 1);
-    });
+    code
+      .replace(/(['"])((:?\\\1|.)+?)\1/g, function(match, quote, str, index) {  // extract all strings
+        _strings.push(str);
+        return '"' + (_strings.length - 1) + '"';
+      })
+      .replace(/\/\*[\s\S]*?\*\//mg, '') // remove block comments
+      .replace(/\/\/.*$/mg, '') // remove line comments
+      .replace(/(?:^|[^.$])\brequire\s*\(\s*"(\d+)"\s*\)/g, function(match, index) {  // parse 'require()'
+        if (index && _strings[index]) {
+          ret.push(_strings[index]);
+        }
+      });
 
-    // remove comments
-    code = code
-      .replace(/\/\*[\s\S]*?\*\//mg, '') // block comments
-      .replace(/\/\/.*$/mg, ''); // line comments
-
-    // parse 'require()'
-    REQUIRE_RE.lastIndex = 0;
-    while ((match = REQUIRE_RE.exec(code))) {
-      if (match[2]) {
-        ret.push(_strings[match[2]])
-      }
-    }
-
-    return util.unique(ret)
+    return util.unique(ret);
   }
 
 })(seajs._util)
