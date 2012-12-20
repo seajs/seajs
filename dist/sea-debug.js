47646695b6739dad320d3fa6770cc26db3a8197c
XiNGRZ <chenxingyu92@gmail.com>
2012-12-20 11:43:53 +0800

    fixed a bug in dependencies parsing.

--- dist/sea-debug.js
+++ dist/sea-debug.js
@@ -690,14 +690,22 @@ seajs._config = {
   util.parseDependencies = function(code) {
     var ret = [], _strings = [];
 
+    var EXTRACT_RE = /(['"])((:?\\\1|.)+?)\1/g
+      , BLOCKCOMMENT_RE = /\/\*[\s\S]*?\*\//mg
+      , LINECOMMENT_RE = /\/\/.*$/mg
+      , REQUIRE_RE = /(?:^|[^.$])\brequire\s*\(\s*"(\d+)"\s*\)/g
+      , FILTER_RE = /^[^"'\s\)]+$/;
+
     code
-      .replace(/(['"])((:?\\\1|.)+?)\1/g, function(match, quote, str, index) {  // extract all strings
+      .replace(EXTRACT_RE, function(match, quote, str, index) {  // extract all strings
         return '"' + (_strings.push(str) - 1) + '"';
       })
-      .replace(/\/\*[\s\S]*?\*\//mg, '') // remove block comments
-      .replace(/\/\/.*$/mg, '') // remove line comments
-      .replace(/(?:^|[^.$])\brequire\s*\(\s*"(\d+)"\s*\)/g, function(match, index) {  // parse 'require()'
-        ret.push(_strings[index]);
+      .replace(BLOCKCOMMENT_RE, '') // remove block comments
+      .replace(LINECOMMENT_RE, '') // remove line comments
+      .replace(REQUIRE_RE, function(match, index) {  // parse 'require()'
+        if (FILTER_RE.test(_strings[index])) {
+          ret.push(_strings[index]);
+        }
       });
 
     return util.unique(ret);
