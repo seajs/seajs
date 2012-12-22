74267bfbfd602702a5a5b217a6b297a60d51fd16
lifesinger <lifesinger@gmail.com>
2012-12-22 14:10:25 +0800

    Improve the regular expression for parsing dependencies

--- dist/sea-debug.js
+++ dist/sea-debug.js
@@ -683,26 +683,17 @@ seajs._config = {
 
 /**
  * The parser for dependencies
+ * Ref: tests/research/parse-dependencies/test.html
  */
 ;(function(util) {
+  var REQUIRE_RE = /"(?:\\"|[^"])*"|'(?:\\'|[^'])*'|\/\*[\S\s]*?\*\/|\/(?:\\\/|[^/])+\/|\/\/.*|(?:^|[^.$])\brequire\s*\(\s*(["'])(.+?)\1\s*\)/g
 
-  var NOISE_RE = /"(?:\\"|[^"])*"|'(?:\\'|[^'])*'|\/\*[\S\s]*?\*\/|\/(?:\\\/|[^/])+\/|\/\/.*/g
-  var STR_RE = /^["']/
-  var REQUIRE_RE = /(?:^|[^.$])\brequire\s*\(\s*(["'])(.+?)\1\s*\)/g
-
-
-  // see ../tests/research/parse-dependencies/test.html
   util.parseDependencies = function(code) {
-    var ret = [], match
-
-    // Remove comments and regexp
-    code = code.replace(NOISE_RE, function(m) {
-      return STR_RE.test(m) ? m : ''
-    })
-
+    var ret = [], m
     REQUIRE_RE.lastIndex = 0
-    while ((match = REQUIRE_RE.exec(code))) {
-      ret.push(match[2])
+
+    while ((m = REQUIRE_RE.exec(code))) {
+      if (m[2]) ret.push(m[2])
     }
 
     return util.unique(ret)
