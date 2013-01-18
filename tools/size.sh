5db1dc6c9ee515dbc3f283deb5d2047034a94899
lifesinger <lifesinger@gmail.com>
2013-01-18 09:31:18 +0800

    Make output format align

--- tools/size.sh
+++ tools/size.sh
@@ -9,6 +9,6 @@ SIZE_MIN=$(cat dist/$MIN | wc -c)
 SIZE_GZIP=$(gzip -nfc --best dist/$MIN | wc -c)
 
 echo "    `echo "scale=3;$SIZE_SRC/1024" | bc -l` bytes $SRC"
-echo "    `echo "scale=3;$SIZE_MIN/1024" | bc -l` bytes $MIN"
+echo "     `echo "scale=3;$SIZE_MIN/1024" | bc -l` bytes $MIN"
 echo "     `echo "scale=3;$SIZE_GZIP/1024" | bc -l` bytes $MIN gzipped"
 echo "  `cat dist/sea-debug.js | wc -l` LOC"
