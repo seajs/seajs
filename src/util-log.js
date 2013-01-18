f73119f76f5828d1d9945e8137f63cefc048fda9
lifesinger <lifesinger@gmail.com>
2013-01-18 15:41:27 +0800

    Improve seajs.log code implement using Function.prototype.bind

--- src/util-log.js
+++ src/util-log.js
@@ -1,41 +1,33 @@
 /**
- * util-log.js - The tiny console
+ * util-log.js - The tiny log function
  */
 
-// Save a reference to the original console
 var console = global.console
 
-// The safe wrapper of console.log/error/...
-function log() {
-  if (typeof console === 'undefined') return
-
-  var args = Array.prototype.slice.call(arguments)
-
-  var type = 'log'
-  var last = args[args.length - 1]
-  console[last] && (type = args.pop())
-
-  // Prints log info in debug mode only.
-  if (type === 'log' && !seajs.debug) return
-
-  if (console[type].apply) {
-    console[type].apply(console, args)
+// The safe wrapper for `console.xxx` functions
+// log('message') ==> console.log('message')
+// log('message', 'warn') ==> console.warn('message')
+var log = seajs.log = function() {
+  if (console === undefined) {
     return
   }
 
-  // ref: issue#349
-  var length = args.length
-  if (length === 1) {
-    console[type](args[0])
+  var args = slice.call(arguments)
+  var len = args.length
+  var type = console[args[len - 1]] ? args.pop() : 'log'
+
+  // Print log info in debug mode only
+  if (type === 'log' && !seajs.debug) {
+    return
   }
-  else if (length === 2) {
-    console[type](args[0], args[1])
-  }
-  else if (length === 3) {
-    console[type](args[0], args[1], args[2])
-  }
-  else {
-    console[type](args.join(' '))
-  }
+
+  var fn = console[type]
+
+  // The console function has no `apply` method in IE9
+  // http://stackoverflow.com/questions/5538972
+  fn = fn.apply ? fn :
+      Function.prototype.bind.call(fn, console)
+  fn.apply(console, args)
 }
+
 
