a527ad15ba4bab8e445392e42b4401d89d8aa87c
lifesinger <lifesinger@gmail.com>
2013-01-21 18:11:55 +0800

    Change debugMode to config.debug

--- src/util-log.js
+++ src/util-log.js
@@ -17,7 +17,7 @@ var log = seajs.log = function() {
   var type = console[args[len - 1]] ? args.pop() : 'log'
 
   // Print log info in debug mode only
-  if (type === 'log' && !debugMode) {
+  if (type === 'log' && !config.debugMode) {
     return
   }
 
