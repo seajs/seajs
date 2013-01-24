449f8105d48b166943bf38b3aeacbb2ba42b7386
lifesinger <lifesinger@gmail.com>
2013-01-24 23:01:43 +0800

    Add test specs for config vars

--- tests/specs/config-vars/main.js
+++ tests/specs/config-vars/main.js
@@ -1,32 +1,23 @@
 
 seajs.config({
   vars: {
-    'sub': './submodule/sub',
-    'sub2': './submodule/sub'
+    'locale': 'zh-cn',
+    'xx': '{brace_in_value}'
   }
-});
+})
+
 
 define(function(require) {
 
-  var test = require('../../test');
-  var a = require('./a');
-  var b = require('./submodule/b');
-  var c = require('{sub}/c');
-  var c2 = require('{sub2}/c2');
-  var d = require('./d');
+  var test = require('../../test')
 
-  test.assert(a.foo == 'a', 'a.foo should equal to "a"');
-  test.assert(b.foo == 'b', 'b.foo should equal to "b"');
-  test.assert(c.foo == 'c', 'c.foo should equal to "c"');
-  test.assert(c2.foo == 'c2', 'c2.foo should equal to "c2"');
-  test.assert(d.foo == 'd', 'd.foo should equal to "d"');
+  var lang = require('./i18n/{locale}')
+  test.assert(lang.name = '中国', lang.name)
 
-  
-  require.async(['../../test', '{sub}/c'], function(test, c) {
+  var resolvedUri = require.resolve('./path/to/{xx}')
+  test.assert(resolvedUri.indexOf('{brace_in_value}') > 0, resolvedUri)
 
-    test.assert((c || 0).foo == 'c', 'c.foo should equal to "c"');
-    test.done();
-
-  });
+  test.done()
 
 });
+
