6ac000def197c5c0ec83fb90de428d3427b415fd
lifesinger <lifesinger@gmail.com>
2013-01-26 21:16:32 +0800

    Simplify config file

--- tests/config.js
+++ tests/config.js
@@ -1,33 +1,9 @@
+define(function(require, exports) {
 
-(function(factory) {
-
-  if (typeof define === 'function') {
-    define(factory)
-  }
-  else if (typeof exports !== 'undefined') {
-    factory(require, exports)
-  }
-  else {
-    factory(null, this)
-  }
-
-})(function(require, exports) {
-
-  exports.testCases = [
-      'specs/util-events/test.html'
-    , 'specs/util-path/test.html'
-
-    , 'specs/config-alias/test.html'
-    , 'specs/config-base'
-    , 'specs/config-charset'
-    , 'specs/config-debug/test.html'
-    , 'specs/config-map'
-    , 'specs/config-preload'
-    , 'specs/config-vars'
-
+  exports.testPages = [
+      'specs/util'
+    , 'specs/config'
   ]
-
-  //exports.testCases = ['unit/test.html']
 
 });
 
