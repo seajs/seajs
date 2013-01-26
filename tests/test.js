1724721029b4c90f27fb55e4f63b63f9b01245bd
lifesinger <lifesinger@gmail.com>
2013-01-26 14:44:01 +0800

    Support multiple specs in one page

--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,46 @@
-(function(factory) {
 
-  if (typeof define === 'function') {
-    define(factory)
-  }
-  else if (typeof exports !== 'undefined') {
-    factory(require, exports)
-  }
-  else {
-    factory(null, (this['test'] = {}))
+// Hack console for testing
+(function(global) {
+  var console = global.console
+  if (!console) return
+
+  console._log = console.log
+  console._warn = console.warn
+
+  console.log = function(arg1, arg2) {
+    var msg = arg2 ? arg1 + ' ' + arg2 : arg1
+    global.consoleMsg = msg
+    console._log(msg)
   }
 
-})(function(require, exports) {
+  console.warn = function(arg1, arg2) {
+    var msg = arg2 ? arg1 + ' ' + arg2 : arg1
+    global.consoleMsg = msg
+    console._warn(msg)
+  }
+})(this)
 
+function printResults(txt, style) {
+  var d = document.createElement('div')
+  d.innerHTML = txt
+  d.className = style
+  document.getElementById('out').appendChild(d)
+}
+
+function printHeader(test, url) {
+  var h = document.createElement('h3')
+  h.innerHTML = test +
+      (url ? ' <a class="hash" href="' + url + '">#</a>' : '')
+  document.getElementById('out').appendChild(h)
+}
+
+
+define(function(require, exports) {
+  var global = this
+  var queue = []
+
+  require.async('./style.css')
+  handleGlobalError()
 
   exports.print = function(txt, style) {
     sendMessage('printResults', txt, style || 'info')
@@ -31,34 +60,55 @@
   }
 
   exports.next = function() {
-    setTimeout(function() {
-      sendMessage('testNext')
-    }, 500) // Leave time for async operation
+    reset()
+
+    if (queue.length) {
+      var id = queue.shift()
+      sendMessage('printHeader', id, getSingleSpecUri(id))
+      seajs.use(configData.base + '../tests/' + id + '/main.js')
+    }
+    else {
+      exports.done()
+    }
+  }
+
+  exports.run = function(ids) {
+    var id = parseIdFromUri()
+    queue = id ? [id] : ids
+    exports.next()
   }
 
   exports.done = function() {
     exports.print('[DONE]')
-    exports.next()
+    sendMessage('testNextPage')
   }
 
 
-  function error(err) {
-    // Firefox will throw an error when script is 404
-    if (err !== 'Error loading script') {
-      exports.print('[ERROR] ' + err, 'error')
+  // Helpers
+
+  var configData = seajs.config.data
+  var defaultConfig = copy(configData, {})
+
+  function reset() {
+    copy(defaultConfig, configData)
+    global.consoleMsg = undefined
+  }
+
+  function copy(from, to) {
+    for (var p in to) {
+      if (to.hasOwnProperty(p)) {
+        delete to[p]
+      }
     }
-    exports.next()
-  }
 
-  // Collect errors in browser environment
-  if (typeof process === 'undefined') {
-    var _onerror = window.onerror
-    window.onerror = function(err) {
-      if (_onerror) _onerror(err)
-      error(err)
+    for (p in from) {
+      if (from.hasOwnProperty(p)) {
+        to[p] = from[p]
+      }
     }
-  }
 
+    return to
+  }
 
   function sendMessage(fn, msg, type) {
     var p = this
@@ -86,4 +136,28 @@
     return '\033[' + ANSI_CODES[type] + 'm  ' + str + '\033[0m'
   }
 
-})
+  function handleGlobalError() {
+    if (typeof window === 'undefined') return
+
+    window.onerror = function(err) {
+      // Ignore 404 error that Firefox will throw when script is not found
+      if (err !== 'Error loading script') {
+        exports.print('[ERROR] ' + err, 'error')
+      }
+
+      // Go on
+      exports.next()
+    }
+  }
+
+  function getSingleSpecUri(id) {
+    return location.href.replace(/\?.*$/, '') + '?' + encodeURIComponent(id)
+  }
+
+  function parseIdFromUri() {
+    return decodeURIComponent(location.search)
+        .replace(/&t=\d+/, '').substring(1)
+  }
+
+});
+
