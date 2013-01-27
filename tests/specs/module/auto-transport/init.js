2f6607fe29269d71d05f6034c944c6fe2ef25b8f
lifesinger <lifesinger@gmail.com>
2013-01-27 19:48:12 +0800

    Add auto-transport specs

--- tests/specs/module/auto-transport/init.js
+++ tests/specs/module/auto-transport/init.js
@@ -1,13 +1,16 @@
 define(function(require) {
 
-  var test = require('../../test')
-  var $ = require('jquery')
-  var cookie = require('cookie')
+  var test = require('../../../test')
+  var $ = require('./jquery')
+  var cookie = require('./jquery.cookie')
 
   test.assert($.fn, '$.fn')
   test.assert(typeof cookie === 'function', typeof cookie)
   test.assert(cookie.defaults, 'cookie.defaults')
 
-  test.done()
+  // clean
+  this['jQuery'] = this.$ = undefined
+
+  test.next()
 
 })
