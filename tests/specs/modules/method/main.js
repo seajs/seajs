cf043022847928e0864b364697c7ced2cc09d62c
lifesinger <lifesinger@gmail.com>
2013-01-27 13:14:30 +0800

    Add method specs

---
+++ tests/specs/modules/method/main.js
@@ -0,0 +1,21 @@
+define(function(require) {
+
+  var test = require('../../../test')
+
+  var a = require('./a')
+  test.assert(a.foo() === a, 'calling a module member')
+
+  var foo = a.foo
+  test.assert(foo() === (function () {
+    return this
+  })(), 'members not implicitly bound')
+
+  a.setX(10)
+  test.assert(a.getX() === 10, 'get and set')
+  test.assert(a.getXFromExports() === a.getX(), 'get from exports')
+
+  test.assert(a.getThis() === this, '`this` is global object')
+
+  test.next()
+
+});
