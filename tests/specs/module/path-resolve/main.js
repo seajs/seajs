13cc35a9c5c4dad333e1f8b8d6dfc5be02510899
lifesinger <lifesinger@gmail.com>
2013-01-27 14:09:31 +0800

    Fix a bug for spec

--- tests/specs/module/path-resolve/main.js
+++ tests/specs/module/path-resolve/main.js
@@ -3,11 +3,11 @@ define(function(require) {
   var test = require('../../../test')
 
   test.assert(require('./missing/missing').bogus === null, 'return null when module file is 404')
-  test.assert(require('./nested/b/c/d').foo() === 1, 'nested module identifier is allowed')
+  test.assert(require('./nested/b/c/d').name === 'd', 'nested module identifier is allowed')
   test.assert(require('./relative/a').foo == require('./relative/b').foo, 'a and b share foo through a relative require')
 
-  var a1 = require('./a.js?v=1.0')
-  var a2 = require('./a.js?v=2.0')
+  var a1 = require('./version/a.js?v=1.0')
+  var a2 = require('./version/a.js?v=2.0')
   test.assert(a1.name === 'a', a1.name)
   test.assert(a2.name === 'a', a2.name)
   test.assert(a1.foo() === 'foo', a1.foo())
