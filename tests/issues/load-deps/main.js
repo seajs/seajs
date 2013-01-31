86e15b43287e987893c980e3b9eb4f96e6278b94
lifesinger <lifesinger@gmail.com>
2013-01-31 10:54:55 +0800

    Add specs for embedded case

--- tests/issues/load-deps/main.js
+++
@@ -1,9 +0,0 @@
-define(function(require) {
-
-  var test = require('../../test');
-
-  test.assert(require('./a2').foo === 'bar', 'augmentPackageHostDeps can be removed');
-  test.assert(require('./a3').foo === 'bar2', 'augmentPackageHostDeps can be removed');
-  test.done();
-
-});
