50d8d41e37914a90631926a723ca5dbb0b2b0ec4
lifesinger <lifesinger@gmail.com>
2013-01-31 12:02:19 +0800

    Delete old specs

--- tests/issues/module-extend/developer.js
+++
@@ -1,9 +0,0 @@
-define(function(require, exports, module) {
-
-  function Developer() {}
-
-  module.extend(Developer, require('./stuff'));
-
-  module.exports = Developer;
-
-});
--- tests/issues/module-extend/person.js
+++
@@ -1,7 +0,0 @@
-define(function(require, exports, module) {
-
-  function Person() {}
-
-  module.exports = Person;
-
-});
