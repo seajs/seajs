08e053c080fcde272582417d80411cef2e8e8fce
lifesinger <lifesinger@gmail.com>
2013-02-01 10:00:27 +0800

    Update specs for seajs.log

--- tests/specs/util/log.js
+++ tests/specs/util/log.js
@@ -1,42 +1,27 @@
 define(function(require) {
 
   var test = require('../../test')
-  var oldIE = !!this.attachEvent
+  var assert = test.assert
 
 
-  oldIE || test.assert(seajs.log('seajs.log test', 'group') === undefined, 'group')
+  assert(seajs.log('seajs.log test', 'group') === undefined, 'group')
 
-  test.assert(seajs.log('a') === undefined, 'log')
-  test.assert(seajs.log('a', 'b') === undefined, 'log')
-  test.assert(seajs.log('a', 'b', 1) === undefined, 'log')
+  assert(seajs.log('a') === undefined, 'log')
+  assert(seajs.log('a', 'b') === undefined, 'log')
+  assert(seajs.log('a', 'b', 1) === undefined, 'log')
 
-  test.assert(seajs.log('a', 'warn') === undefined, 'warn')
-  test.assert(seajs.log('a', 'info') === undefined, 'info')
-  test.assert(seajs.log('a', 'error') === undefined, 'error')
-  test.assert(seajs.log('a', 'time') === undefined, 'time')
-  test.assert(seajs.log('a', 'timeEnd') === undefined, 'timeEnd')
+  assert(seajs.log('a', 'warn') === undefined, 'warn')
+  assert(seajs.log('a', 'info') === undefined, 'info')
+  assert(seajs.log('a', 'error') === undefined, 'error')
+  assert(seajs.log('a', 'time') === undefined, 'time')
+  assert(seajs.log('a', 'timeEnd') === undefined, 'timeEnd')
 
-  oldIE || test.assert(seajs.log('a', 'dir') === undefined, 'dir')
-  oldIE || test.assert(seajs.log({ name: 'a' }, 'dir') === undefined, 'dir')
+  assert(seajs.log('a', 'dir') === undefined, 'dir')
+  assert(seajs.log({ name: 'a' }, 'dir') === undefined, 'dir')
 
-  oldIE || test.assert(seajs.log('seajs.log test', 'groupEnd') === undefined, 'groupEnd')
+  assert(seajs.log('seajs.log test', 'groupEnd') === undefined, 'groupEnd')
 
+  test.next()
 
-  // 补足用例数，使得各个浏览器下一致
-  if (oldIE) {
-    test.assert(true, 'seajs.log')
-    test.assert(true, 'seajs.log')
-    test.assert(true, 'seajs.log')
-    test.assert(true, 'seajs.log')
-  }
+});
 
-
-  // ref: #503
-  seajs.debug = true
-  this.console = { log: seajs.log }
-  console.log('hello')
-
-
-  test.done()
-
-})
