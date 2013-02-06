906f46c62a116358b41ee279f377fff5259f5729
leeight <leeight@gmail.com>
2013-02-06 22:07:12 +0800

    add examples/showcase

---
+++ examples/showcase/circular_deps/c.js
@@ -0,0 +1,43 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2013 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * c.js ~ 2013/02/05 20:56:25
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ *  
+ **/
+define(function(require, exports, module){
+  var a = require("./a");
+  var d = require("./d");
+
+  exports.name = "[c" + ',' + a.name + ',' + d.name + "]";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
---
+++ examples/showcase/circular_deps/e.js
@@ -0,0 +1,40 @@
+/***************************************************************************
+ * 
+ * Copyright (c) 2013 Baidu.com, Inc. All Rights Reserved
+ * $Id$ 
+ * 
+ **************************************************************************/
+ 
+ 
+ 
+/**
+ * e.js ~ 2013/02/05 20:55:47
+ * @author leeight(liyubei@baidu.com)
+ * @version $Revision$ 
+ * @description 
+ *  
+ **/
+define(function(require, exports, module){
+  exports.name = "[e]";
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* vim: set ts=4 sw=4 sts=4 tw=100: */
