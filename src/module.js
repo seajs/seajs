5f7e5c53520f6741ea53cd43200983035624ea99
lifesinger <lifesinger@gmail.com>
2013-02-07 14:06:35 +0800

    Remove require.cache

--- src/module.js
+++ src/module.js
@@ -298,8 +298,6 @@ function compile(mod) {
     return resolve(id, mod.uri)
   }
 
-  require.cache = cachedModules
-
 
   var factory = mod.factory
   var exports = factory === undefined ? mod.exports : factory
