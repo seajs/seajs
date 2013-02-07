9aacccefa85699a6f140e1db621ddc83df2290b1
lifesinger <lifesinger@gmail.com>
2013-02-07 21:37:52 +0800

    Move seajs-find specs to plugin-debug folder

--- tests/specs/extensible/plugin-debug/main.js
+++ tests/specs/extensible/plugin-debug/main.js
@@ -10,6 +10,18 @@ define(function(require) {
 
   test.assert(document.getElementById('seajs-debug-console'), 'console div')
 
+
+  require('./seajs-find/xx-1')
+  require('./seajs-find/xx-2')
+
+  test.assert(seajs.find('seajs-find/xx-1.js').length === 1, 'seajs.find')
+  test.assert(seajs.find('seajs-find/xx-1.js')[0].name === '1', 'seajs.find')
+  test.assert(seajs.find('seajs-find/xx').length === 2, 'seajs.find')
+  test.assert(seajs.find('seajs-find/xx')[0].name === '1', 'seajs.find')
+  test.assert(seajs.find('seajs-find/xx')[1].name === '2', 'seajs.find')
+  test.assert(seajs.find('seajs-find/zz').length === 0, 'seajs.find')
+
+
   require.async('./c', function(c) {
     test.assert(c.name === 'c', c.name)
 
