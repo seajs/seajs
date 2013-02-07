93123c83feb4aa0d60b7d3894f338eaa8a8fc6c2
lifesinger <lifesinger@gmail.com>
2013-02-07 21:19:19 +0800

    Move plugin-debug to extensible folder

--- tests/config.js
+++ tests/config.js
@@ -5,11 +5,11 @@ var testPages = [
   , 'specs/config'
   , 'specs/module'
   , 'specs/extensible'
+  , 'specs/extensible/plugin-debug'
   , 'specs/package'
 
   , 'specs/misc/data-api'
   , 'specs/misc/ie-cache'
-  , 'specs/misc/plugin-debug'
   , 'specs/misc/utf8-in-gbk'
   , 'specs/misc/x-ua-compatible'
 ]
