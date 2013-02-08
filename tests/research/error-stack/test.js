c2ff9930b47a3f740aa8bafcb133990dc0a4be3b
lifesinger <lifesinger@gmail.com>
2013-02-08 14:27:24 +0800

    Improve script for error-stack test page

--- tests/research/error-stack/test.js
+++ tests/research/error-stack/test.js
@@ -3,8 +3,11 @@ try {
   o.m.g.w.t.f()
 }
 catch(ex) {
+
   for(var p in ex) {
-    test.print('ex.' + p + ' = ' + ex[p])
+    if (!/^(?:message|stack)$/.test(p)) {
+      test.print('ex.' + p + ' = ' + ex[p])
+    }
   }
 
   test.print('ex.message = ' + ex.message)
