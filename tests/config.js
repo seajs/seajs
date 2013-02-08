6a36565de7abee60ca1657f4138bccd6fcb6ea7e
lifesinger <lifesinger@gmail.com>
2013-02-08 22:19:04 +0800

    Add research/derive-uri to config

--- tests/config.js
+++ tests/config.js
@@ -4,13 +4,15 @@ var testPages = [
 
   , 'specs/config'
   , 'specs/module'
+  , 'specs/package'
   , 'specs/extensible'
   , 'specs/extensible/plugin-debug'
-  , 'specs/package'
 
   , 'specs/misc/data-api'
   , 'specs/misc/ie-cache'
   , 'specs/misc/utf8-in-gbk'
   , 'specs/misc/x-ua-compatible'
+
+  , 'research/derive-uri'
 ]
 
