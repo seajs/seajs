2a13367651fb5add49b7b4010a166667183f6ce3
lifesinger <lifesinger@gmail.com>
2013-02-09 11:13:36 +0800

    Update test specs according to code modification

--- tests/specs/util/log.js
+++ tests/specs/util/log.js
@@ -4,7 +4,7 @@ define(function(require) {
   var assert = test.assert
 
 
-  assert(seajs.log('seajs.log test', 'group') === undefined, 'group')
+  //assert(seajs.log('seajs.log test', 'group') === undefined, 'group')
 
   assert(seajs.log('a') === undefined, 'log')
   assert(seajs.log('a ' + 1) === undefined, 'log')
@@ -12,13 +12,13 @@ define(function(require) {
   assert(seajs.log('a', 'warn') === undefined, 'warn')
   assert(seajs.log('a', 'info') === undefined, 'info')
   assert(seajs.log('a', 'error') === undefined, 'error')
-  assert(seajs.log('a', 'time') === undefined, 'time')
-  assert(seajs.log('a', 'timeEnd') === undefined, 'timeEnd')
+  //assert(seajs.log('a', 'time') === undefined, 'time')
+  //assert(seajs.log('a', 'timeEnd') === undefined, 'timeEnd')
 
   assert(seajs.log('a', 'dir') === undefined, 'dir')
   assert(seajs.log({ name: 'a' }, 'dir') === undefined, 'dir')
 
-  assert(seajs.log('seajs.log test', 'groupEnd') === undefined, 'groupEnd')
+  //assert(seajs.log('seajs.log test', 'groupEnd') === undefined, 'groupEnd')
 
   test.next()
 
