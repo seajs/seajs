31563617a38749720fc11aa71e9c833ab124ab3b
lifesinger <lifesinger@gmail.com>
2013-02-14 21:00:11 +0800

    Add test runner for Node.js

---
+++ tests/runner-node.js
@@ -0,0 +1,36 @@
+/**
+ * Run test specs in node environment
+ * Usage:
+ *   $ cd seajs
+ *   $ node tests/runner-node.js
+ */
+
+require("../lib/sea-node")
+var test = require("./test")
+
+var specs = [
+  "module/anonymous",
+  "module/anywhere",
+  "module/cache",
+  "module/chain",
+  "module/circular",
+  "module/define",
+  "module/dependencies",
+  "module/duplicate-load",
+  "module/singleton",
+  "module/exports",
+  "module/jsonp",
+  "module/load-css",
+  "module/math",
+  "module/method",
+  "module/multi-load",
+  "module/multi-versions",
+  "module/override",
+  "module/public-api",
+  "module/require-async",
+  "module/transitive"
+]
+
+// go
+test.run(specs)
+
