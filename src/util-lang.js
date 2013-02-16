47474d826bc98ce25f6e8d21637039457284a505
lifesinger <lifesinger@gmail.com>
2013-02-16 08:26:45 +0800

    Remove unique and simplify code with isType function

--- src/util-lang.js
+++ src/util-lang.js
@@ -2,33 +2,14 @@
  * util-lang.js - The minimal language enhancement
  */
 
-var hasOwnProperty = {}.hasOwnProperty
-
-function hasOwn(obj, prop) {
-  return hasOwnProperty.call(obj, prop)
-}
-
-function isFunction(obj) {
-  return typeof obj === "function"
-}
-
-var isArray = Array.isArray || function(obj) {
-  return obj instanceof Array
-}
-
-function unique(arr) {
-  var obj = {}
-  var ret = []
-
-  for (var i = 0, len = arr.length; i < len; i++) {
-    var item = arr[i]
-    if (obj[item] !== 1) {
-      obj[item] = 1
-      ret.push(item)
-    }
+function isType(type) {
+  return function(obj) {
+    return Object.prototype.toString.call(obj) === "[object " + type + "]"
   }
-
-  return ret
 }
+
+var isObject = isType("Object")
+var isArray = Array.isArray || isType("Array")
+var isFunction = isType("Function")
 
 
