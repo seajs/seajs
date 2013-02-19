527f8729572352ca9bf382f8bab99deaedb78a44
lifesinger <lifesinger@gmail.com>
2013-02-19 19:40:51 +0800

    Comment use strict

--- src/intro.js
+++ src/intro.js
@@ -2,7 +2,12 @@
  * @preserve SeaJS v@VERSION | seajs.org/LICENSE.md
  */
 (function(global, undefined) {
-"use strict"
+
+// Can't do this because several apps including ASP.NET trace
+// the stack via arguments.caller.callee and Firefox dies if
+// you try to trace through "use strict" call chains. (jquery#13335)
+// Support: Firefox 18+
+//"use strict"
 
 // Avoid conflicting when `sea.js` is loaded multiple times
 var _seajs = global.seajs
