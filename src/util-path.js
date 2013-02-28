e32a73317a16f40b6c3190e1585a25546ab8603c
lifesinger <lifesinger@gmail.com>
2013-02-28 09:32:19 +0800

    Improve dirname(path)

--- src/util-path.js
+++ src/util-path.js
@@ -2,15 +2,13 @@
  * util-path.js - The utilities for operating path such as id, uri
  */
 
-var DIRNAME_RE = /[^?#]*(?=\/.*$)/
+var DIRNAME_RE = /[^?#]*\//
 
 // Extract the directory portion of a path
 // dirname("a/b/c.js?t=123#xx/zz") ==> "a/b/"
-// dirname("d.js") ==> "./"
 // ref: http://jsperf.com/regex-vs-split/2
 function dirname(path) {
-  var s = path.match(DIRNAME_RE)
-  return (s ? s[0] : ".") + "/"
+  return path.match(DIRNAME_RE)[0]
 }
 
 
