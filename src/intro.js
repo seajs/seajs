c936419a4260ebe419cea40721fa07c0fc191330
lifesinger <lifesinger@gmail.com>
2013-03-01 18:22:16 +0800

    Remove use strict comments

--- src/intro.js
+++ src/intro.js
@@ -3,12 +3,6 @@
  */
 (function(global, undefined) {
 
-// Can't do this because several apps including ASP.NET trace
-// the stack via arguments.caller.callee and Firefox dies if
-// you try to trace through "use strict" call chains. (jquery#13335)
-// Support: Firefox 18+
-//"use strict"
-
 // Avoid conflicting when `sea.js` is loaded multiple times
 var _seajs = global.seajs
 if (_seajs && _seajs.version) {
