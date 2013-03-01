3928bd050d8d0e480afa556d40f41c2a3aa3805a
lifesinger <lifesinger@gmail.com>
2013-03-01 18:08:53 +0800

    Simpify plugin-nocache

--- src/plugins/plugin-nocache.js
+++ src/plugins/plugin-nocache.js
@@ -11,21 +11,8 @@
     var requestUri = data.requestUri || data.uri
 
     if (requestUri.indexOf(noCachePrefix) === -1) {
-      requestUri += (requestUri.indexOf("?") === -1 ? "?" : "&")
+      data.requestUri += (requestUri.indexOf("?") === -1 ? "?" : "&")
           + noCacheTimeStamp
-    }
-
-    data.requestUri = requestUri
-  })
-
-  // Restore the original uri in automatically deriving case
-  seajs.on("resolve", function(data) {
-    var requestUri = data.id
-
-    if (requestUri.indexOf(noCachePrefix) === -1) {
-      data.uri = requestUri
-          .replace(noCachePrefix, "")
-          .replace(/[?&]$/, "")
     }
   })
 
