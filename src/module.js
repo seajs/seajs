46c422716323f4785eedd430e8045fef7d8e85ba
lifesinger <lifesinger@gmail.com>
2013-03-03 18:24:37 +0800

    Simplify arguments logic of define function

--- src/module.js
+++ src/module.js
@@ -162,23 +162,10 @@ function fetch(uri, callback) {
 }
 
 function define(id, deps, factory) {
-  var argsLen = arguments.length
-
-  // HANDLE: define(factory)
-  if (argsLen === 1) {
+  // define(factory)
+  if (arguments.length === 1) {
     factory = id
     id = undefined
-  }
-  // HANDLE: define(id || deps, factory)
-  else if (argsLen === 2) {
-    factory = deps
-    deps = undefined
-
-    // HANDLE: define(deps, factory)
-    if (isArray(id)) {
-      deps = id
-      id = undefined
-    }
   }
 
   // Try to derive uri in IE6-9 for anonymous modules
