635d57255cc22ea07bbfe16622ba5a3fa6a2d77f
lifesinger <lifesinger@gmail.com>
2013-03-11 11:57:18 +0800

    Modify specs for shim

--- tests/specs/extensible/mutable-shim/main.js
+++ tests/specs/extensible/mutable-shim/main.js
@@ -15,16 +15,16 @@ seajs.config({
 seajs.on("resolve", function(data) {
   var id = data.id || ""
   var m = id.match(/^angular_(\w+)$/)
-return;
-  if (m && !seajs.config.data.alias[m[0]]) {
-    var shim = {}
 
-    shim[m[0]] = {
+  if (m && !seajs.config.data.alias[m[0]]) {
+    var alias = {}
+
+    alias[m[0]] = {
       src: 'angular/' + m[1] + '.js',
       deps: ['jquery']
     }
 
-    seajs.config({ alias: shim })
+    seajs.config({ alias: alias })
   }
 })
 
