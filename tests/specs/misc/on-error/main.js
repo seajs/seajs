bc24c83daef21248ee0ab141675bd6170c399403
lifesinger <lifesinger@gmail.com>
2013-04-01 18:34:26 +0800

    Improve specs for error event

--- tests/specs/misc/on-error/main.js
+++ tests/specs/misc/on-error/main.js
@@ -34,7 +34,7 @@ define(function(require) {
 
   function done() {
     if (++n === 3) {
-      test.assert(w_errors.length === 2, w_errors.length)
+      test.assert(w_errors.length > 0, w_errors.length)
       test.assert(s_errors.length === 4, s_errors.length)
       test.next()
     }
