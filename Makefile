bb6332f4db38a40c7196be6296bf790620720ea2
lifesinger <lifesinger@gmail.com>
2013-04-23 09:30:50 +0800

    Tweak code style

--- Makefile
+++ Makefile
@@ -9,14 +9,14 @@ build_all:
 
 test: test_node test_local test_http
 
-test_http:
-	@node tools/server.js . phantomjs tools/phantom.js http://127.0.0.1:9012/tests/runner.html?console
+test_node:
+	@node tests/node-runner.js
 
 test_local:
 	@phantomjs tools/phantom.js tests/runner.html?console
 
-test_node:
-	@node tests/node-runner.js
+test_http:
+	@node tools/server.js . phantomjs tools/phantom.js http://127.0.0.1:9012/tests/runner.html?console
 
 totoro:
 	@totoro-test --adapter=tests/totoro-adapter.js
--- tools/server.js
+++ tools/server.js
@@ -1,45 +1,46 @@
-var fs = require('fs');
-var http = require('http');
-var print = require('util').print;
-var spawn = require('child_process').spawn;
-var static = require('node-static');
+
+var fs = require('fs')
+var http = require('http')
+var print = require('util').print
+var spawn = require('child_process').spawn
+var Static = require('node-static')
+
 
 function createServer(dir, cmd) {
-  dir = dir || '.';
+  dir = dir || '.'
 
-  // Server Start
-  var file = new static.Server(fs.realpathSync(dir));
+  var file = new Static.Server(fs.realpathSync(dir))
 
   var server = http.createServer(function(request, response) {
-    request.addListener('end', function() {
-      file.serve(request, response);
-    }).resume();
-  });
+    request.addListener('end',function() {
+      file.serve(request, response)
+    }).resume()
+  })
 
   server.listen(9012, function() {
     if (cmd) {
-      var args = cmd.split(' ');
+      var args = cmd.split(' ')
       args = args.map(function(arg) {
-        return arg.trim();
-      });
+        return arg.trim()
+      })
       args = args.filter(function(arg) {
-        return arg.length;
-      });
-      var runner = spawn(args[0], args.slice(1));
+        return arg.length
+      })
+      var runner = spawn(args[0], args.slice(1))
       runner.stdout.on('data', function(data) {
-        print(data.valueOf());
-      });
+        print(data.valueOf())
+      })
       runner.on('exit', function(code) {
         if (code === 127) {
-          print(cmd + ' not available');
+          print(cmd + ' not available')
         }
-        server.close();
-        process.exit(code);
-      });
+        server.close()
+        process.exit(code)
+      })
     }
-  });
+  })
 }
 
-var dir = process.argv[2] || '.';
-var cmd = process.argv.slice(3);
-createServer(dir, cmd.join(' '));
+var dir = process.argv[2] || '.'
+var cmd = process.argv.slice(3)
+createServer(dir, cmd.join(' '))
