644c82238cb8a8e549c661a2f2c509df9f538bde
lifesinger <lifesinger@gmail.com>
2013-06-12 14:02:01 +0800

    Comment rare case

--- tests/specs/config/base/main.js
+++ tests/specs/config/base/main.js
@@ -38,20 +38,6 @@ define(function(require) {
       'actual = ' + require.resolve('z')
           + ' expected = ' + expectedPath('z'))
 
-
-  // rare but allowed case
-  seajs.config({
-    base: '/'
-  })
-
-  base = seajs.data.base
-  var href = (global.location || {}).href
-
-  test.assert(!href || // For Node.js
-      href.indexOf('file://') === 0 ||
-      href.indexOf(base) === 0, base)
-
-
   test.next()
 
 })
