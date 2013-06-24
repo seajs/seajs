c841f1f6d832c165378ed3e03ac5be53ce8607dd
lifesinger <lifesinger@gmail.com>
2013-06-24 16:00:53 +0800

    Fix specs for config

--- tests/specs/util/config.js
+++ tests/specs/util/config.js
@@ -6,13 +6,13 @@ define(function(require) {
 
   // var BASE_RE = /^(.+?\/)(\?\?)?(seajs\/)+/
 
-  assert(BASE_RE.match('http://test.com/seajs/sea.js')[1] === 'http://test.com/', 'BASE_RE')
-  assert(BASE_RE.match('http://test.com/seajs/2.1.0/sea.js')[1] === 'http://test.com/', 'BASE_RE')
-  assert(BASE_RE.match('http://test.com/seajs/seajs/2.1.0/sea.js')[1] === 'http://test.com/', 'BASE_RE')
+  assert('http://test.com/seajs/sea.js'.match(BASE_RE)[1] === 'http://test.com/', 'BASE_RE')
+  assert('http://test.com/seajs/2.1.0/sea.js'.match(BASE_RE)[1] === 'http://test.com/', 'BASE_RE')
+  assert('http://test.com/seajs/seajs/2.1.0/sea.js'.match(BASE_RE)[1] === 'http://test.com/', 'BASE_RE')
 
-  assert(BASE_RE.match('http://test.com/??seajs/seajs/2.1.0/sea.js,jquery/jquery/1.9.2/jquery.js')[1] === 'http://test.com/', 'BASE_RE')
-  assert(BASE_RE.match('http://test.com/seajs/??seajs/2.1.0/sea.js,seajs-combo/1.0.0/seajs-combo.js')[1] === 'http://test.com/', 'BASE_RE')
-  assert(BASE_RE.match('http://test.com/libs/??seajs/seajs/2.1.0/sea.js,jquery/jquery/1.9.2/jquery.js')[1] === 'http://test.com/libs/', 'BASE_RE')
+  assert('http://test.com/??seajs/seajs/2.1.0/sea.js,jquery/jquery/1.9.2/jquery.js'.match(BASE_RE)[1] === 'http://test.com/', 'BASE_RE')
+  assert('http://test.com/seajs/??seajs/2.1.0/sea.js,seajs-combo/1.0.0/seajs-combo.js'.match(BASE_RE)[1] === 'http://test.com/', 'BASE_RE')
+  assert('http://test.com/libs/??seajs/seajs/2.1.0/sea.js,jquery/jquery/1.9.2/jquery.js'.match(BASE_RE)[1] === 'http://test.com/libs/', 'BASE_RE')
 
 
 
