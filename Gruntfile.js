fcb9ab60ee71bff4efee9af91f6f325eaf2bb4ba
lifesinger <lifesinger@gmail.com>
2013-08-08 16:56:40 +0800

    Use uglify-js instead of gcc

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,51 +8,48 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON("package.json"),
 
     concat: {
-      seajs: {
+      dist: {
         src: [
           "src/intro.js",
           "src/sea.js",
-
           "src/util-lang.js",
           "src/util-events.js",
           "src/util-path.js",
           "src/util-request.js",
           "src/util-deps.js",
-
           "src/module.js",
           "src/config.js",
-
           "src/outro.js"
         ],
-        dest: "dist/sea-debug.js"
+        dest: 'dist/sea-debug.js'
       }
     },
 
-    gcc: {
-      seajs: {
-        src: "dist/sea-debug.js",
-        dest: "dist/sea.js",
+    uglify: {
+      all: {
+        files: {
+          "dist/sea.js": ["dist/sea-debug.js"]
+        },
         options: {
           banner: "/*! Sea.js <%= pkg.version %> | seajs.org/LICENSE.md\n" +
-              "//# sourceMappingURL=sea.js.map\n*/",
-
-          source_map_format: "V3",
-          create_source_map: "dist/sea.js.map",
-
-          compilation_level: "SIMPLE_OPTIMIZATIONS",
-          externs: "tools/extern.js",
-
-          warning_level: "VERBOSE",
-          jscomp_off: "checkTypes",
-          jscomp_error: "checkDebuggerStatement"
+              "//@ sourceMappingURL=sea.js.map\n*/\n",
+          sourceMap: "dist/sea.js.map",
+          sourceMappingURL: "sea.js.map",
+          //report: "gzip",
+          compress: {
+            unsafe: true,
+            unused: false
+          },
+          mangle: {
+          }
         }
       }
     }
-
   })
 
 
-  grunt.registerTask("embed", "Embed version etc.", function() {
+  // Replace @VERSION tokens to real values
+  grunt.registerTask("post-concat", function() {
     var filepath = "dist/sea-debug.js"
     var version = grunt.config("pkg.version")
 
@@ -60,40 +57,34 @@ module.exports = function(grunt) {
     code = code.replace(/@VERSION/g, version)
     grunt.file.write(filepath, code)
 
-    grunt.log.writeln("@VERSION is replaced to \"" + version + "\".")
+    grunt.log.writeln('"@VERSION" is replaced to "' + version + '".')
   })
 
-  grunt.registerTask("fix", "Fix sourceMap etc.", function() {
+  // Fix sourceMap after compressing
+  grunt.registerTask("post-uglify", "Fix sourceMap etc.", function() {
     var mapfile = "dist/sea.js.map"
 
     var code = grunt.file.read(mapfile)
-    code = code.replace('"file":""', '"file":"sea.js"')
+    code = code.replace('"file":"dist/sea.js"', '"file":"sea.js"')
     code = code.replace("dist/sea-debug.js", "sea-debug.js")
     grunt.file.write(mapfile, code)
     grunt.log.writeln('"' + mapfile + '" is fixed.')
 
-    /*
-    // No `$` variable in compressed code to avoiding conflicting
-    // when inline in velocity template.
     var minfile = "dist/sea.js"
-
     code = grunt.file.read(minfile)
-    code = code.replace('function $', 'function _')
-    code = code.replace('$=', '_=')
-    code = code.replace(/\$\./g, '_.')
-    code = code.replace(/\$&&/g, '_&&')
-    code = code.replace(/=\$/g, '=_')
-    code = code.replace(/\$\(/g, '_(')
+    code = code.replace(/\/\*\n\/\/@ sourceMappingURL=sea\.js\.map\n\*\//, "")
     grunt.file.write(minfile, code)
-    grunt.log.writeln('$ in "' + minfile + '" is fixed.')
-    */
+    grunt.log.writeln('"' + minfile + '" is fixed.')
   })
 
 
-  grunt.loadTasks("tools/grunt-tasks")
+  // Load grunt tasks from NPM packages
   grunt.loadNpmTasks("grunt-contrib-concat")
+  grunt.loadNpmTasks("grunt-contrib-uglify")
 
-  grunt.registerTask("default", ["concat", "embed", "gcc:seajs", "fix"])
+
+  grunt.registerTask("default",
+      ["concat", "post-concat", "uglify", "post-uglify"])
 
 }
 
