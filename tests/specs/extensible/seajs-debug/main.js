bae98aca07992526add6cb6215633148afe44d13
qiaohua <shengyan1985@gmail.com>
2013-08-27 14:54:34 +0800

    U: fix seajs-debug test case

--- tests/specs/extensible/seajs-debug/main.js
+++ tests/specs/extensible/seajs-debug/main.js
@@ -2,11 +2,9 @@ define(function(require) {
 
   var test = require('../../../test')
 
-  var a = require('./a')
-  var b = require('./b')
+  var a = require('./a-debug')
 
   test.assert(a.name === 'a', a.name)
-  test.assert(b.name === 'b', b.name)
 
   test.assert(document.getElementById('seajs-debug-console'), 'console div')
 
@@ -20,14 +18,5 @@ define(function(require) {
   test.assert(seajs.find('seajs-find/xx')[0].name === '1', 'seajs.find')
   test.assert(seajs.find('seajs-find/xx')[1].name === '2', 'seajs.find')
   test.assert(seajs.find('seajs-find/zz').length === 0, 'seajs.find')
-
-  //console.log(require.resolve('./c'))
-
-  require.async('./c', function(c) {
-    test.assert(c.name === 'c', c.name)
-
-    document.cookie = 'seajs-debug=0``0; path=/; expires=' + new Date(0)
-    test.next()
-  })
 
 })
