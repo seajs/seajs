56618877f692a65e21d04c37392208a1baf4fc69
army8735 <army8735@qq.com>
2014-04-25 15:49:22 +0800

    a typo

--- tests/specs/package/multi-entry/main.js
+++ tests/specs/package/multi-entry/main.js
@@ -34,13 +34,13 @@ define(function(require) {
     }
   })
 
-  // clone a and be, append a new use
+  // clone a and b, append a new use
   seajs.use('./multi-entry/a1', function(a) {
     test.assert(a === 'b', 'module should return "b"')
     success()
   })
   seajs.on("request", function(data) {
-    if(data.uri.indexOf("b.js") > -1) {
+    if(data.uri.indexOf("b1.js") > -1) {
       seajs.use('./multi-entry/a1', function(a) {
         test.assert(a === 'b', 'module should return "b"')
         success()
@@ -67,19 +67,19 @@ define(function(require) {
   })
 
   // clone c d e, append a new use
-  seajs.use('./multi-entry/c', function(c) {
+  seajs.use('./multi-entry/c1', function(c) {
     test.assert(c === 'e', 'module should return "e"')
     success()
   })
   seajs.on("request", function(data) {
-    if(data.uri.indexOf("d.js") > -1) {
-      seajs.use('./multi-entry/c', function(c) {
+    if(data.uri.indexOf("d1.js") > -1) {
+      seajs.use('./multi-entry/c1', function(c) {
         test.assert(c === 'e', 'module should return "e"')
         success()
       })
     }
   })
-  seajs.use('./multi-entry/c', function(c) {
+  seajs.use('./multi-entry/c1', function(c) {
     test.assert(c === 'e', 'module should return "e"')
     success()
   })
