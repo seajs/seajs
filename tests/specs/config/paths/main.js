4dc24970960a562317e19e40f6c5d7fdc605f467
army8735 <army8735@qq.com>
2014-05-22 11:03:44 +0800

    add config test
    paths with -

--- tests/specs/config/paths/main.js
+++ tests/specs/config/paths/main.js
@@ -5,7 +5,8 @@ seajs.config({
   },
   paths: {
     'biz': './path/to/biz',
-    'to': './path/to/'
+    'to': './path/to/',
+    'hy-phen': './path/to/biz'
   }
 })
 
@@ -16,9 +17,11 @@ define(function(require) {
 
   var a = require('to/a')
   var b = require('b')
+  var d = require('hy-phen/d')
 
   test.assert(a.name === 'a', a.name)
   test.assert(b.name === 'b', b.name)
+  test.assert(d.name === 'd', d.name)
 
   require.async('to/c', function(c) {
     test.assert(c.name === 'c', c.name)
