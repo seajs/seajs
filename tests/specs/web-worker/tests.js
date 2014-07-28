f5fe8bfbb23e915f8b423bf01a1f50cc403c91e6
AKFish <akfish@gmail.com>
2014-07-28 19:43:51 +0800

    Add test for web worker

---
+++ tests/specs/web-worker/tests.js
@@ -0,0 +1,26 @@
+define(function(require) {
+  function assert(value, message) {
+    self.postMessage({message: message, result: value});
+  }
+  console.log("Start runner in worker")
+  self.addEventListener('message', function (e) {
+    console.log("On message: " + e.data);
+    if (e.data === 'start') {
+      console.log("Start running tests");
+      var a = require('a');
+      assert(a === 'a', 'a is a');
+
+      var b = require('b')
+      assert(JSON.stringify(b) == '{}', 'b is {}')
+
+      var c = require('c')
+      assert(JSON.stringify(c) == '{}', 'c is {}')
+
+      assert(b === c, 'b returns c')
+
+
+      var e = require('e')
+      assert(e === 'e', 'e is e')
+    }
+  })
+})
