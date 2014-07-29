fece5afa5c36d56b8ed008d6177f7c3fd7cf6225
AKFish <akfish@gmail.com>
2014-07-29 14:46:31 +0800

    Revert util-cs.js #1279

--- dist/runtime-debug.js
+++ dist/runtime-debug.js
@@ -406,34 +406,6 @@ if (isBrowser) {
     }
   }
 
-  // Note: originally in util-cs.js
-  //       it referenced several temp variable from util-request.js (this file)
-  //       don't see why not putting them together
-  function getCurrentScript() {
-    if (currentlyAddingScript) {
-      return currentlyAddingScript
-    }
-
-    // For IE6-9 browsers, the script onload event may not fire right
-    // after the script is evaluated. Kris Zyp found that it
-    // could query the script nodes and the one that is in "interactive"
-    // mode indicates the current script
-    // ref: http://goo.gl/JHfFW
-    if (interactiveScript && interactiveScript.readyState === "interactive") {
-      return interactiveScript
-    }
-
-    var scripts = head.getElementsByTagName("script")
-
-    for (var i = scripts.length - 1; i >= 0; i--) {
-      var script = scripts[i]
-      if (script.readyState === "interactive") {
-        interactiveScript = script
-        return interactiveScript
-      }
-    }
-  }
-
   // For Developers
   seajs.request = request
 
@@ -450,6 +422,8 @@ if (isBrowser) {
   }
   // For Developers
   seajs.request = requestFromWebWorker;
+} else {
+  seajs.request = function() {}
 }
 
 /**
