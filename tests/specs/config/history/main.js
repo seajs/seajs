e4ed6f32b2dd9bef45eabe1bf11a5519bcccc58d
army8735 <army8735@qq.com>
2015-01-24 11:44:35 +0800

    more tests #1414

--- tests/specs/config/history/main.js
+++ tests/specs/config/history/main.js
@@ -16,6 +16,39 @@ define(function(require) {
   test.assert(seajs.data.history.alias.length == 2)
   test.assert(seajs.data.history.alias[0].a == 'x')
   test.assert(seajs.data.history.alias[1].a == 'y')
+
+  seajs.config({
+    map: [
+      ['a', 'a1'],
+      function(){return 1}
+    ]
+  })
+  seajs.config({
+    map: [
+      ['b', 'b1'],
+      function(){return 2}
+    ]
+  })
+
+  test.assert(seajs.data.history.map.length == 2)
+  test.assert(seajs.data.history.map[0].length == 2)
+  test.assert(seajs.data.history.map[1].length == 2)
+  test.assert(seajs.data.history.map[0][0].join(',') == 'a,a1')
+  test.assert(seajs.data.history.map[0][1].toString().indexOf('return 1') > -1)
+  test.assert(seajs.data.history.map[1][0].join(',') == 'b,b1')
+  test.assert(seajs.data.history.map[1][1].toString().indexOf('return 2') > -1)
+
+  seajs.config({
+    combo: /reg1/
+  })
+  seajs.config({
+    combo: /reg2/
+  })
+
+  test.assert(seajs.data.history.combo.length == 2)
+  test.assert(seajs.data.history.combo[0].toString() == '/reg1/')
+  test.assert(seajs.data.history.combo[1].toString() == '/reg2/')
+
   test.next()
 
 })
